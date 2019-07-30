432c0e8618f0a0b1e156ab082ec589fe910730ed
Marcello Vartuli <mvartuli@gmail.com>
2019-07-30 11:54:13 -0300

    Começando a implementar o tabuleiro

---
+++ xadrez-console/NTabuleiro/Cor.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Ntabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+}
---
+++ xadrez-console/NTabuleiro/Tabuleiro.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Ntabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+
+        }
+    }
+}
